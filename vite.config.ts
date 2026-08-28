3e4e54a3a43ae60e5e661e4a9967d93b3f16cb64
LEANDRO422874365 <leandro27082006@gmail.com>
2026-08-27 22:07:38 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/leandro-solucoes-digitais/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
